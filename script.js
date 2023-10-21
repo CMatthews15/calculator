b42a8935c200229832f1bac8aa066060b73c16c1
cmatthews15 <chastitydmatthews@gmail.com>
2023-10-20 23:25:40 -0400

    final html/css

---
+++ script.js
@@ -0,0 +1,33 @@
+/* set all variables first 
+data-number
+data-operator
+data-clear
+data-equal*/
+
+
+class calc {
+    constructor(output){
+        this.output = output
+    }
+}
+
+function appendNumber(number)
+
+function chooseOperation(operation)
+
+function clear()
+
+function equal()
+
+function compute() 
+
+function updateDisplay()
+//19:04 on youtube video https://www.youtube.com/watch?v=j59qQ7YWLxw
+
+
+const output = document.querySelector("[data-output")
+const numButton = document.querySelectorAll("[data-number]")
+const opButton = document.querySelectorAll("[data-operator]")
+const clearButton = document.querySelector("[data-clear]")
+const equalButton = document.querySelector("[data-equal]")
+
